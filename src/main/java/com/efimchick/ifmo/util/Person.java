2b9b0b14ffe4106e8e73be136d53c6adf20c70df
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-11-14 21:01:19 +0300

    Fixing SonarLint problems

--- src/main/java/com/efimchick/ifmo/util/Person.java
+++ src/main/java/com/efimchick/ifmo/util/Person.java
@@ -29,8 +29,12 @@ public class Person {
 
     @Override
     public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         final Person person = (Person) o;
         return age == person.age &&
                 Objects.equals(firstName, person.firstName) &&
